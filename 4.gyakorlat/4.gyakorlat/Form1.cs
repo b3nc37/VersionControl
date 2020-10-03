57a57bb2cfc35e9cedcced39184d1f866d909119
Bence Tamás Németh <bence.nemeth4@stud.uni-corvinus.hu>
2020-10-03 14:59:45 +0200

    6.feladat

--- 4.gyakorlat/4.gyakorlat/Form1.cs
+++ 4.gyakorlat/4.gyakorlat/Form1.cs
@@ -17,10 +17,15 @@ namespace _4.gyakorlat
     {
         List<Flat> Flats;
         RealEstateEntities context = new RealEstateEntities();
+        Excel.Application xlApp; 
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet; 
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
+            
         }
 
 
@@ -28,5 +33,40 @@ namespace _4.gyakorlat
         {
            List<Flat> Flats = context.Flat.ToList();
         }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                
+                xlApp = new Excel.Application();
+
+               
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                //CreateTable(); 
+
+                
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
+        }
+
     }
 }
