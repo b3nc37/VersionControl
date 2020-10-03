0ed8721713b75d9d016ad7a26c92085bca6593d4
Bence Tamás Németh <bence.nemeth4@stud.uni-corvinus.hu>
2020-10-03 21:54:39 +0200

    7.feladat

--- 4.gyakorlat/4.gyakorlat/Form1.cs
+++ 4.gyakorlat/4.gyakorlat/Form1.cs
@@ -9,14 +9,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.Drawing.Text;
 
 namespace _4.gyakorlat
 {
 
     public partial class Form1 : Form
     {
-        List<Flat> Flats;
+        
         RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         Excel.Application xlApp; 
         Excel.Workbook xlWB; 
         Excel.Worksheet xlSheet; 
@@ -31,7 +33,7 @@ namespace _4.gyakorlat
 
         private void LoadData()
         {
-           List<Flat> Flats = context.Flat.ToList();
+           Flats = context.Flat.ToList();
         }
 
         private void CreateExcel()
@@ -47,8 +49,8 @@ namespace _4.gyakorlat
                 
                 xlSheet = xlWB.ActiveSheet;
 
-                // Tábla létrehozása
-                //CreateTable(); 
+                
+                CreateTable(); 
 
                 
                 xlApp.Visible = true;
@@ -66,6 +68,77 @@ namespace _4.gyakorlat
                 xlApp = null;
             }
 
+            
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Egy szobára jutó ár"
+            };
+
+            for (int x = 0; x < headers.Length; x++)
+            {
+                xlSheet.Cells[1, 1] = headers[0];
+                xlSheet.Cells[1, 2] = headers[1];
+                xlSheet.Cells[1, 3] = headers[2];
+                xlSheet.Cells[1, 4] = headers[3];
+                xlSheet.Cells[1, 5] = headers[4];
+                xlSheet.Cells[1, 6] = headers[5];
+                xlSheet.Cells[1, 7] = headers[6];
+                xlSheet.Cells[1, 8] = headers[7];
+                xlSheet.Cells[1, 9] = headers[8];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+
+            foreach (var x in Flats)
+            {
+                values[counter, 0] = x.Code;
+                values[counter, 1] = x.Vendor;
+                values[counter, 2] = x.Side;
+                values[counter, 3] = x.District;
+                values[counter, 4] = x.Elevator;
+                values[counter, 5] = x.NumberOfRooms;
+                values[counter, 6] = x.FloorArea;
+                values[counter, 7] = x.Price;
+                values[counter, 8] ="="+GetCell(counter+2,8)+"/"+GetCell(counter+2,6);
+                counter++;
+
+            }
+
+            xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
 
     }
