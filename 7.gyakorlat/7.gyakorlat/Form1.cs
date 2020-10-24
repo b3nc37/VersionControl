35517a7e532804a2eb8db46e87b2040f150968c1
Bence Tamás Németh <bence.nemeth4@stud.uni-corvinus.hu>
2020-10-24 12:24:27 +0200

    7.feladat

--- 7.gyakorlat/7.gyakorlat/Form1.cs
+++ 7.gyakorlat/7.gyakorlat/Form1.cs
@@ -31,15 +31,49 @@ namespace _7.gyakorlat
 
 
         }
+        private void SimStep(int year, Person person)
+        {
+            
+            if (!person.IsAlive) return;
 
-        public void Szimulacio()
+            
+            byte age = (byte)(year - person.BirthYear);
+
+            
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+            
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+               
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+        }
+
+            public void Szimulacio()
         {
             for (int year = 2005; year <= 2024; year++)
             {
 
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    // Ide jön a szimulációs lépés
+                    SimStep(year,Population[i]);
                 }
 
                 int nbrOfMales = (from x in Population
